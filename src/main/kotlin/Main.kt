691ad529d5d2d19e16e22564f5a0eb4085715823
Vitaly Saprygin <82073769+Vsaprygin@users.noreply.github.com>
2022-12-03 12:10:17 +0700

    Update Main.kt
    add fun "printAnimalAll"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,27 +5,29 @@ fun main() {
     val animal3 = Carni("Песец", "2500",35)
     val animal4 = Carni("Норка", "3600", 15)
 
-    printHerbs(animal1)
-    printHerbs(animal2)
-    printCarnis(animal3)
-    printCarnis(animal4)
+    printAnimalAll(animal1)
+    printAnimalAll(animal2)
+    printAnimalAll(animal3)
+    printAnimalAll(animal4)
+
 }
 fun printHerbs (animal: Herb){
-    printAnimal("${animal::class.qualifiedName}")
     animal.printName()
     animal.printHerb()
 }
 
 fun printCarnis (animal: Carni ){
-    printAnimal("${animal::class.qualifiedName}")
     animal.printName()
     animal.printCarni()
 }
-fun printAnimal(className:String){
+fun printAnimalAll (animal: Any){
+    val className =  animal::class.qualifiedName
     if (className == "Herb"){
         print("Травоядное ")
+        printHerbs(animal as Herb)
     }
     if (className == "Carni"){
         print("Плотоядное  ")
+        printCarnis(animal as Carni)
     }
 }
